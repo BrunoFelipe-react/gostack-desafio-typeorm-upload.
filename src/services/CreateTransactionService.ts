56ac820029ffdb3e34d5f97f173991bcfa13ec58
Bruno <brunoynu@gmail.com>
2020-12-17 20:06:21 -0300

    App

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
-
+import AppError from '../errors/AppError';
+import {getCustomRepository,getRepository} from 'typeorm';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type : 'income' | 'outcome';
+  category:string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({title,value,type,category}:Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const {total} = await transactionsRepository.getBalance();
+
+    if(type === 'outcome' && total < value){
+      throw new AppError("Você não possui saldo suficiente")
+    }
+
+    
+
+    let transactionCategory = await categoryRepository.findOne({
+      where:{
+        title : category
+      }
+    });
+
+    if(!transactionCategory){
+      transactionCategory = categoryRepository.create({
+        title:category
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+    
+    let transaction = new Transaction();
+    transaction = transactionsRepository.create({
+      title,
+      value,  
+      type,
+      category:transactionCategory
+    });
+    console.log(transaction);
+    transaction = await transactionsRepository.save(transaction);
+    console.log(transaction);
+    return transaction;
+
   }
 }
 
